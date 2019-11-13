11e2f59f54fa2431555d547324b2d93b81125729
Nick Foden <nicfoden@gmail.com>
2019-11-13 17:04:10 -0500

    running queries

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Auth from "aws-amplify";
+import { Auth, API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
-
+import * as queries from "./graphql/queries";
 Auth.configure(awsconfig);
+API.configure(awsconfig);
 
 function App() {
+  const allTodos = API.graphql(graphqlOperation(queries.listTodos));
+  console.log(allTodos);
+
+  const oneTodo = API.graphql(
+    graphqlOperation(queries.getTodo, {
+      id: "33611216-cde4-4814-b358-a275573feac7"
+    })
+  );
+  console.log(oneTodo);
+
   return (
     <div className="App">
       <header className="App-header">
